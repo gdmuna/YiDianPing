d0b116c4cd42032475dca2f08154d7caf5d695e0
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-07-30 22:59:57 +0800

    feat：“编辑个人资料”页面demo

---
+++ yidianping-web/src/pages/user/setMyself.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-full max-w-screen-sm h-full mx-auto px-10 py-10">
+        <var-card class="" title="编辑个人资料">
+            <template #extra>
+                <var-space>
+                    <var-button type="primary">编辑个人资料</var-button>
+                </var-space>
+            </template>
+        </var-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage',
+    components: {},
+    setup() {
+        return {};
+    },
+    data() {
+        return {};
+    },
+    created() {},
+    mounted() {},
+    updated() {},
+    methods: {}
+};
+</script>
+
+<style scoped></style>
