2e3742e5d5c171eddc563129f29441c01f890b8d
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-07-30 22:40:10 +0800

    feat：连接更多对评论的操作的相关接口

--- yidianping-web/src/api/comment.js
+++ yidianping-web/src/api/comment.js
@@ -13,6 +13,22 @@ const Comment = {
     //恢复评论
     recoverComment(data) {
         return request.post(`${moduleUrl}/recoverComment`, data);
+    },
+    //查询历史评论
+    getHistoryComment(data) {
+        return request.get(`${moduleUrl}/getHistoryComment`, data);
+    },
+    //点赞评论
+    ThumbsUp(data) {
+        return request.put(`${moduleUrl}/ThumbsUp`, data);
+    },
+    //取消点赞
+    cancelThumbsUp(data) {
+        return request.put(`${moduleUrl}/cancelThumbsUp`, data);
+    },
+    //查询点赞的评论
+    getLoveComment(data) {
+        return request.get(`${moduleUrl}/getLoveComment`, data);
     }
 };
 export default Comment;
