b297c18ace234fc4bbdb632ed420024e6f9825b7
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-07-30 22:53:47 +0800

    feat：“创建新评论”页面demo

---
+++ yidianping-web/src/pages/user/createComment.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-full max-w-screen-sm h-full mx-auto px-10 py-10">
+        <var-card class="" title="创建新评论">
+            <template #extra>
+                <var-space>
+                    <var-button type="primary">发布</var-button>
+                </var-space>
+            </template>
+        </var-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage',
+    components: {},
+    setup() {
+        return {};
+    },
+    data() {
+        return {};
+    },
+    created() {},
+    mounted() {},
+    updated() {},
+    methods: {}
+};
+</script>
+
+<style scoped></style>
