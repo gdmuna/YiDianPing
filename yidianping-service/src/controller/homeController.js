351c65ae2974292054fb576184adfcf2db461655
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-01 15:48:07 +0800

    fix：controller获取评论信息接口注释

--- yidianping-service/src/controller/homeController.js
+++ yidianping-service/src/controller/homeController.js
@@ -7,16 +7,12 @@ const homeService = require('../service/homeService');
  * @name commentInfo 获取评论信息
  * @description GET /home/commentInfo
  * @header {string} authorization 用户凭证
- * @response {Object} commentInfo 用户信息
+ * @response {Object} commentInfo 评论信息
  * @response {string} commentInfo.userId 用户ID
- * @response {string} commentInfo.account 用户账号
+ * @response {string} commentInfo.avatar 用户头像
  * @response {string} commentInfo.nickName 用户昵称
+ * @response {string} commentInfo.score 用户对评论对象的评分
  */
-// router.get('/commentInfo', async (req, res, next) => {
-//     const { commentId } = req.query;
-//     const commentInfo = await homeService.getCommentInfo(commentId);
-//     res.ResultVO(0, '成功', commentInfo);
-// });
 router.get('/commentInfo', async (req, res, next) => {
     const commentInfo = await homeService.getCommentInfo();
     res.ResultVO(0, '成功', commentInfo);
