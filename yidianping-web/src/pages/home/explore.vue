f89faf1c04acd65729b3ec62b1b0ecd610d1c213
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-02 20:12:55 +0800

    fix：”首页“及其子页顶部导航选项卡样式修改

--- yidianping-web/src/pages/home/explore.vue
+++ yidianping-web/src/pages/home/explore.vue
@@ -1,5 +1,5 @@
 <template>
-    <var-tabs v-model:active="activeChildrenTabs" class="sticky top-0" style="--tab-font-size: 1.05rem; --tab-active-font-size: 1.05rem; border-bottom: 1px solid rgb(242, 245, 248)" color="rgb(255, 255, 255)" active-color="#4E77B9" inactive-color="#4E77B9" indicator-color="#ffffff">
+    <var-tabs v-model:active="activeChildrenTabs" class="sticky top-0" style="--tab-font-size: 1rem; --tab-active-font-size: 1.05rem; border-bottom: 1px solid rgb(242, 245, 248)" color="rgb(255, 255, 255)" active-color="#0F46A0" inactive-color="#4E77B9" indicator-color="#ffffff">
         <var-tab name="三饭" class="mt-0.5" @click="goTheThirdCanteen">三饭</var-tab>
     </var-tabs>
     <div class="children-show-area" style="overflow-y: scroll">
@@ -34,8 +34,8 @@ export default {
 <style scoped>
 .children-show-area {
     width: 100%;
-    height: calc(100% - 193px);
+    height: calc(100% - 173px);
     position: fixed;
-    top: 129px;
+    top: 109px;
 }
 </style>
