cc428cda7211cfd87664c46d063796b2e835588c
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-02 20:13:34 +0800

    fix：”我的“页面顶部导航选项样式修改

--- yidianping-web/src/pages/user/user.vue
+++ yidianping-web/src/pages/user/user.vue
@@ -2,13 +2,13 @@
     <var-app-bar fixed safe-area-top style="background-color: #ffffff; color: #0f46a0; box-shadow: none; --app-bar-left-gap: 1rem; --app-bar-right-gap: 1rem">
         <!-- 设置按钮 -->
         <template #right>
-            <var-button type="primary" class="h-10 w-10 mr-1 mt-12" color="#ffffff" style="border-radius: 1.1rem; box-shadow: inset 0 0 4px rgb(159, 178, 234)" @click="goSettings">
+            <var-button type="primary" class="h-10 w-10 mr-1 mt-8" color="#ffffff" style="border-radius: 1.1rem; box-shadow: inset 0 0 4px rgb(159, 178, 234)" @click="goSettings">
                 <font-awesome-icon :icon="['far', 'sun']" size="xl" style="color: #2041a9" />
             </var-button>
         </template>
         <!-- 基本信息+选项卡 -->
         <template #content>
-            <div class="flex gap-1 items-center mt-5">
+            <div class="flex gap-1 items-center mt-3">
                 <!-- 头像+昵称+修改个人 -->
                 <var-space direction="column" class="ml-8 self-end">
                     <var-avatar src="https://krseoul.imgtbl.com/i/2024/07/29/66a7b7f6ec0b6.jpg" fit="cover" size="68" />
@@ -17,18 +17,18 @@
                 <var-button type="primary" class="h-10 rounded-2xl mr-5 pt-1" color="#ffffff" style="font-size: 0.95rem; letter-spacing: 0.02rem; box-shadow: inset 0 0 4px rgb(159, 178, 234); color: #4e77b9" @click="goSetMyself">编辑资料</var-button>
             </div>
             <!-- "我的页面"选项卡 -->
-            <var-tabs v-model:active="activeTabs" style="margin-top: 0.9rem; --tab-font-size: 0.9rem; --tab-active-font-size: 0.9rem; --tab-active-color: #d5e4fb" color="FFFFFF" active-color="#4E77B9" inactive-color="#4E77B9">
+            <var-tabs v-model:active="activeTabs" style="margin-top: 0.5rem; --tab-font-size: 1.05rem; --tab-active-font-size: 1.1rem; --tab-active-color: #d5e4fb" color="FFFFFF" active-color="#4E77B9" inactive-color="#4E77B9">
                 <var-tab name="我评论的" @click="goMyComment">
                     <font-awesome-icon :icon="['far', 'comment-dots']" size="lg" class="mr-1" style="color: #4e77b9" />
-                    我评论的
+                    <p style="margin-top: 1px">我评论的</p>
                 </var-tab>
                 <var-tab name="收藏" @click="goCollect">
                     <font-awesome-icon :icon="['far', 'star']" size="lg" class="mr-1" style="color: #4e77b9" />
-                    收藏
+                    <p style="margin-top: 2px">收藏</p>
                 </var-tab>
                 <var-tab name="点赞" @click="goLike">
                     <font-awesome-icon :icon="['far', 'heart']" size="lg" class="mr-1" style="color: #4e77b9" />
-                    点赞
+                    <p style="margin-top: 1px">点赞</p>
                 </var-tab>
             </var-tabs>
         </template>
@@ -79,8 +79,8 @@ export default {
 <style scoped>
 .show-area {
     width: 100%;
-    height: calc(100% - 280px);
+    height: calc(100% - 263.6px);
     position: fixed;
-    top: 214px;
+    top: 199.6px;
 }
 </style>
