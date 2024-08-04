6a4a47f30f386e760b5bf45e73c914f7b3dc8f86
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-04 16:45:25 +0800

    feat：“创建新评论”——“选择评论对象”页面

---
+++ yidianping-web/src/pages/user/selectSubject.vue
@@ -0,0 +1,89 @@
+<template>
+    <!-- 顶部导航栏 -->
+    <var-app-bar fixed safe-area-top title-position="center" class="relative top-0" style="background-color: #f6f8fb; color: #0f46a0; box-shadow: none; --app-bar-height: 64px">
+        <div class="flex w-[100vw] justify-between items-center px-[1%]">
+            <!-- 返回上一页面按钮 -->
+            <var-button round text color="transparent" text-color="#0F46A0" class="" @click="goBack">
+                <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" style="color: #2041a9" />
+            </var-button>
+            <!-- 搜索框 -->
+            <div class="w-[75%]" style="margin: 0 auto">
+                <input type="text" class="w-full h-[2rem] rounded-[20px] p-2.5 focus:outline-none" style="border: 1px solid #9ab1d7" />
+            </div>
+            <!-- 搜索按钮 -->
+            <var-button round text color="transparent" text-color="#0F46A0" class="mt-1" @click="goBack">
+                <p class="t text-base">搜索</p>
+            </var-button>
+        </div>
+    </var-app-bar>
+    <div style="margin-bottom: 12%" class="px-[4%] pb-[4%]">
+        <div v-for="(card, index) in filteredComments" :key="index" class="mb-2" @click="selectSubject(card)">
+            <div class="flex w-full h-[82px] mb-3 p-[9px] bg-white" style="border-radius: 4px">
+                <div class="w-full flex justify-between">
+                    <div class="flex">
+                        <var-image :src="card.cb_img ? card.cb_img : 'https://krseoul.imgtbl.com/i/2024/08/02/66accb57ab412.png'" :radius="5" height="4rem" width="5.5rem" fit="cover" class="mr-3" />
+                        <p class="mt-0.5">{{ card.cb_title }}</p>
+                    </div>
+                    <div class="flex flex-col items-end justify-between mr-1.5">
+                        <p class="text-sm" style="color: #0f46a0; margin-top: 2.5px">{{ card.avg_score && card.avg_score > 0 ? card.avg_score + ' 分' : '暂无评分' }}</p>
+                        <p class="text-sm" style="color: #999999; margin-top: 2.5px">{{ card.comment_count }} 人已评论</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="flex justify-center mt-4 text-xs" style="color: #999999">没有更多内容咯</p>
+    </div>
+</template>
+
+<script>
+import home from '@/api/home';
+
+export default {
+    name: 'SelectSubjectPage',
+    components: {},
+    setup() {
+        return {};
+    },
+    data() {
+        return {
+            cards: [],
+            commentSubjectInfo: {
+                avg_score: '',
+                cb_img: '',
+                cb_title: ''
+            }
+        };
+    },
+    computed: {
+        filteredComments() {
+            return this.cards.filter((card) => card.is_enabled == 0);
+        }
+    },
+    created() {
+        this.getCommentSubjectInfo();
+        document.addEventListener('backbutton', this.goBack);
+    },
+    mounted() {},
+    unmounted() {
+        document.removeEventListener('backbutton', this.goBack);
+    },
+    updated() {},
+    methods: {
+        goBack() {
+            this.$router.go(-1);
+        },
+        async getCommentSubjectInfo() {
+            const commentSubjectInfo = await home.getCommentSubjectInfo();
+            this.cards = commentSubjectInfo;
+        },
+        selectSubject(card) {
+            // 将选中的店铺信息存储到 localStorage
+            localStorage.setItem('selectedSubject', JSON.stringify(card));
+            // 返回上一页面
+            this.$router.go(-1);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
