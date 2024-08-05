982a37f9a8a864fad732aa7ec991b5999c616d2a
xierfloat <164899869+dfwgj@users.noreply.github.com>
2024-08-05 22:18:34 +0800

    feat：设置页功能完善

--- yidianping-web/src/pages/user/settings.vue
+++ yidianping-web/src/pages/user/settings.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="center-container">
         <var-space direction="column" class="button-container">
-            <var-button class="custom-button" block @click="onaccout()">
+            <var-button class="custom-button" block @click="goaccout">
                 <div class="button-content">
                     <font-awesome-icon :icon="['far', 'circle-user']" size="xl" style="color: #3246a0" />
                     <span>账号安全</span>
@@ -10,7 +10,7 @@
                     <font-awesome-icon :icon="['fas', 'chevron-right']" size="lg" style="color: #0f46a0" />
                 </div>
             </var-button>
-            <var-button class="custom-button" block @click="onnotice()">
+            <var-button class="custom-button" block @click="onnotice = true">
                 <div class="button-content">
                     <font-awesome-icon :icon="['far', 'envelope']" size="xl" style="color: #0f46a0" />
                     <span>通知设置</span>
@@ -19,7 +19,7 @@
                     <font-awesome-icon :icon="['fas', 'chevron-right']" size="lg" style="color: #0f46a0" />
                 </div>
             </var-button>
-            <var-button class="custom-button" block @click="onmanager()">
+            <var-button class="custom-button" block @click="gomanager">
                 <div class="button-content">
                     <font-awesome-icon :icon="['fas', 'user-tie']" size="xl" style="color: #0f46a0" />
                     <span>管理员页</span>
@@ -28,7 +28,7 @@
                     <font-awesome-icon :icon="['fas', 'chevron-right']" size="lg" style="color: #0f46a0" />
                 </div>
             </var-button>
-            <var-button class="custom-button" block @click="onmyself()">
+            <var-button class="custom-button" block @click="onmyself">
                 <div class="button-content">
                     <font-awesome-icon :icon="['far', 'face-smile']" size="xl" style="color: #0f46a0" />
                     <span>关于我们</span>
@@ -37,7 +37,7 @@
                     <font-awesome-icon :icon="['fas', 'chevron-right']" size="lg" style="color: #0f46a0" />
                 </div>
             </var-button>
-            <var-button class="custom-button" block @click="show = true">
+            <var-button class="custom-button" block @click="exitDialog = true">
                 <div class="button-content">
                     <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" size="xl" style="color: #0f46a0" />
                     <span>退出登录</span>
@@ -48,21 +48,91 @@
             </var-button>
         </var-space>
     </div>
-    <var-dialog title="兰亭序" message="兰亭临帖 行书如行云流水" v-model:show="show" @confirm="Snackbar.success('confirm')" @cancel="Snackbar.error('cancel')" @closed="Snackbar.info('closed')" />
+    <!-- 通知设置 -->
+    <var-dialog v-model:show="onnotice" :title="onnoticeTitle" :message="onnoticeMessage" style="border-radius: 10px" @confirm="toggleNotification" @cancel="closeNoticeDialog" @closed="closeNoticeDialog" />
+    <!-- 管理员弹窗 -->
+    <var-dialog v-model:show="showDialog" title="抱歉" :message="dialogMessage" style="border-radius: 10px" @confirm="onDialogConfirm" @cancel="onDialogCancel" @closed="onDialogClosed" />
+    <!-- 退出登录 -->
+    <var-dialog v-model:show="exitDialog" title="退出" message="确定要退出登录吗？" style="border-radius: 10px" @confirm="exitConfirm" @cancel="exitCancel" @closed="exitClosed" />
 </template>
 
 <script>
+import user from '../../api/user';
 export default {
     name: 'UserPage',
-    setup() {
+    data() {
         return {
-            show: false
+            users: {
+                avatar: '',
+                nickname: '',
+                account: '',
+                is_admin: ''
+            },
+            showDialog: false,
+            exitDialog: false,
+            onnotice: false,
+            notificationsEnabled: true, // 默认开启通知
+            onnoticeTitle: '消息推送',
+            onnoticeMessage: '是否关闭消息推送？',
+            dialogMessage: ''
         };
     },
-    data() {
-        return {};
+    created() {
+        this.fetchUser();
     },
-    methods: {}
+    methods: {
+        async fetchUser() {
+            const response = await user.getUser({ userId: '0190c9e4-684a-7070-a326-d0b5c07d65b0' });
+            this.users = response;
+        },
+        onmyself() {
+            window.location.href = 'https://mp.weixin.qq.com/s/55oNmN9ufBYXlGxsqhw3fA';
+        },
+        gomanager() {
+            if (this.users.is_admin === 0) {
+                this.dialogMessage = '你没有该权限';
+                this.showDialog = true;
+            } else {
+                this.$router.push('/manager');
+            }
+        },
+        toggleNotificationDialog() {
+            if (this.notificationsEnabled) {
+                this.onnoticeMessage = '是否关闭消息推送？';
+            } else {
+                this.onnoticeMessage = '是否开启消息推送？';
+            }
+            this.onnotice = true;
+        },
+        toggleNotification() {
+            this.notificationsEnabled = !this.notificationsEnabled;
+            this.onnotice = false;
+        },
+        closeNoticeDialog() {
+            this.onnotice = false;
+        },
+        exitConfirm() {
+            this.$router.push('/login');
+        },
+        goaccout() {
+            this.$router.push('/account');
+        },
+        exitCancel() {
+            this.exitDialog = false;
+        },
+        exitClosed() {
+            this.exitDialog = false;
+        },
+        onDialogConfirm() {
+            this.showDialog = false;
+        },
+        onDialogCancel() {
+            this.showDialog = false;
+        },
+        onDialogClosed() {
+            this.showDialog = false;
+        }
+    }
 };
 </script>
 
@@ -72,7 +142,7 @@ export default {
     display: flex; /* 使用弹性盒子布局 */
     justify-content: center; /* 水平居中子元素 */
     align-items: center; /* 垂直居中子元素 */
-    height: 70vh; /* 容器的高度设置为视口高度的55% */
+    height: 56vh; /* 容器的高度设置为视口高度的55% */
 }
 
 /* 定义一个自定义按钮类，用于设置按钮的样式 */
