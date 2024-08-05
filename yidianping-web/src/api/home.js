8b3928cd856bfe8b6ad2cb561dcae22af71eeb80
Zollin0 <145448984+Zollin0@users.noreply.github.com>
2024-08-05 17:37:06 +0800

    feat：”根据评论主体id获取对应评论主体信息“接口连接

--- yidianping-web/src/api/home.js
+++ yidianping-web/src/api/home.js
@@ -10,6 +10,10 @@ const home = {
     // 查询评论对象详细信息
     getCommentSubjectInfo() {
         return request.get(`${moduleUrl}/commentSubjectInfo`);
+    },
+    // 根据评论主体id获取对应评论主体信息
+    getTheCommentSubjectInfo(comtSubjectId) {
+        return request.get(`${moduleUrl}/theCommentSubjectInfo`, { comtSubjectId });
     }
 };
 
