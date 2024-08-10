2f0bfa9f50388fd813c3ee6353acdcc1f2a091da
xierfloat <164899869+dfwgj@users.noreply.github.com>
2024-08-10 21:09:30 +0800

    feat：后端提问回复修改提交实现

--- yidianping-service/src/service/questionService.js
+++ yidianping-service/src/service/questionService.js
@@ -15,3 +15,13 @@ exports.recoverQuestion = async (comtSubjectId, questionId) => {
     const Question = await questionDao.recoverQuestion(comtSubjectId, questionId);
     return Question;
 };
+//发布提问回复
+exports.putQuestionReply = async (comtSubjectId, questionId, text, userId, imgPath) => {
+    const putQuestionReply = await questionDao.putQuestionReply(comtSubjectId, questionId, text, userId, imgPath);
+    return putQuestionReply;
+};
+//修改提问回复
+exports.updateQuestionReply = async (questionId, text, commentId, userId, imgPath) => {
+    const updateQuestionReply = await questionDao.updateQuestionReply(questionId, text, commentId, userId, imgPath);
+    return updateQuestionReply;
+};
