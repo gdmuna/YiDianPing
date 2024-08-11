5052777c470a5969e93b8b506065a1be1232a1f7
xierfloat <164899869+dfwgj@users.noreply.github.com>
2024-08-12 02:51:11 +0800

    fix:管理员页面调整

--- yidianping-web/src/pages/manager/manager.vue
+++ yidianping-web/src/pages/manager/manager.vue
@@ -2,28 +2,39 @@
     <a-layout class="layout-demo">
         <a-layout-sider hide-trigger="true" collapsible :collapsed="collapsed">
             <div class="logo-container">
+                <!-- 头像要换成动态的，头像为空时显示默认头像 -->
                 <img src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" width="40px" height="40px" alt="logo" />
                 <div class="managername">
-                    欢迎回来！
-                    <br />
-                    管理员 吴总
+                    <!-- 吴总要换成动态的 -->
+                    <p>欢迎回来！</p>
+                    <p>管理员 吴总</p>
                 </div>
             </div>
-            <a-menu :default-open-keys="['1']" :default-selected-keys="['0_3']" :style="{ width: '100%' }" @menu-item-click="onClickMenuItem">
-                <a-menu-item key="用户信息" @click="gousergl">
+            <a-menu :style="{ width: '100%' }" @menu-item-click="onClickMenuItem">
+                <a-menu-item @click="gousergl">
                     <IconCalendar />
                     用户信息
                 </a-menu-item>
-                <a-sub-menu key="评论体">
+                <a-sub-menu>
                     <template #title>
                         <span>
                             <IconCalendar />
                             评论体
                         </span>
                     </template>
-                    <a-menu-item key="评论体信息" @click="gocomtbody">评论体信息</a-menu-item>
-                    <a-menu-item key="评论信息" @click="gocomt">评论信息</a-menu-item>
-                    <a-menu-item key="提问信息" @click="goquestion">提问信息</a-menu-item>
+                    <a-menu-item @click="gocommentSubject">评论体信息</a-menu-item>
+                    <a-menu-item @click="gocomment">评论信息</a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu>
+                    <template #title>
+                        <span>
+                            <IconCalendar />
+                            评论体问答
+                        </span>
+                    </template>
+                    <a-menu-item @click="goquestion">提问信息</a-menu-item>
+                    <!-- 页面跳转 -->
+                    <a-menu-item @click="goreply">回答信息</a-menu-item>
                 </a-sub-menu>
             </a-menu>
         </a-layout-sider>
@@ -36,7 +47,8 @@
             </a-layout-header>
             <a-layout style="padding: 0 24px">
                 <a-breadcrumb :style="{ margin: '16px 0' }">
-                    <a-breadcrumb-item>返回登录</a-breadcrumb-item>
+                    <!-- 两个按钮点击跳转到对应页面，首页为管理员欢迎页 -->
+                    <a-breadcrumb-item @click="goLogin">返回登录</a-breadcrumb-item>
                     <a-breadcrumb-item>首页直达</a-breadcrumb-item>
                 </a-breadcrumb>
                 <a-layout-content>
@@ -49,53 +61,44 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { Message } from '@arco-design/web-vue';
 import { IconCaretRight, IconCaretLeft, IconCalendar } from '@arco-design/web-vue/es/icon';
-
-export default defineComponent({
+export default {
     name: 'ManagerHtml',
     components: {
         IconCaretRight,
         IconCaretLeft,
         IconCalendar
     },
-    setup() {
-        const router = useRouter();
-        const collapsed = ref(false);
-
-        const onCollapse = () => {
-            collapsed.value = !collapsed.value;
-        };
-
-        const onClickMenuItem = (key) => {
-            Message.info({ content: `You select ${key}`, showIcon: true });
-        };
-        const gousergl = () => {
-            router.push('/manager/usergl');
-        };
-        const gocomtbody = () => {
-            router.push('/manager/commentSubjectgl');
-        };
-        const gocomt = () => {
-            router.push('/manager/commentgl');
-        };
-        const goquestion = () => {
-            router.push('/manager/questiongl');
-        };
-
+    data() {
         return {
-            collapsed,
-            onCollapse,
-            onClickMenuItem,
-            gousergl,
-            gocomtbody,
-            gocomt,
-            goquestion
+            collapsed: false,
+            subMenuOpen: false
         };
+    },
+    methods: {
+        onCollapse() {
+            this.collapsed = !this.collapsed;
+        },
+        toggleSubMenu() {
+            this.subMenuOpen = !this.subMenuOpen;
+        },
+        gousergl() {
+            this.$router.push('/manager/usergl');
+        },
+        gocommentSubject() {
+            this.$router.push('/manager/commentSubjectgl');
+        },
+        gocomment() {
+            this.$router.push('/manager/commentgl');
+        },
+        goquestion() {
+            this.$router.push('/manager/questiongl');
+        },
+        goLogin() {
+            this.$router.push('/login');
+        }
     }
-});
+};
 </script>
 
 <style scoped>
--- yidianping-web/src/pages/manager/usergl.vue
+++ yidianping-web/src/pages/manager/usergl.vue
@@ -1,86 +1,89 @@
 <template>
-    <a-table :data="currentTableData" :pagination="false" :bordered="{ cell: true }" style="height: 100%" :scroll="{ x: 2000 }" size="large">
-        <template #columns>
-            <a-table-column title="账号" data-index="account" fixed="left" width="150"></a-table-column>
-            <a-table-column title="昵称" data-index="nickname" width="150"></a-table-column>
-            <a-table-column title="头像" width="50">
-                <template #cell="{ record }">
-                    <a-button size="mini" status="warning" type="text" @click="imgUser(record)">查看</a-button>
-                </template>
-            </a-table-column>
-            <a-table-column title="创建时间" data-index="created_at" width="400"></a-table-column>
-            <a-table-column title="操作" width="100" fixed="right">
-                <template #cell="{ record }">
-                    <a-popconfirm content="是否要暂时删除?" ok-text="是" cancel-text="否" @ok="banUser(record)">
-                        <a-button v-if="record.is_forbidden === 0" size="mini" status="danger" type="text">封禁</a-button>
-                    </a-popconfirm>
-                    <a-popconfirm content="是否要恢复此用户?" ok-text="是" cancel-text="否" @ok="unbanUser(record)">
-                        <a-button v-if="record.is_forbidden === 1" size="mini" status="success" type="text">恢复</a-button>
-                    </a-popconfirm>
-                    <a-button size="mini" status="danger" type="text" @click="editUser(record)">修改</a-button>
-                    <a-button size="mini" status="warning" type="text" @click="setUp(record)">设置</a-button>
-                </template>
-            </a-table-column>
-            <a-table-column title="状态" width="50" data-index="is_forbidden" fixed="right">
-                <template #cell="{ record }">
-                    <a-button v-if="record.is_forbidden === 0" status="success" size="mini" type="outline">正常</a-button>
-                    <a-button v-if="record.is_forbidden !== 0" status="danger" size="mini" type="outline">封禁</a-button>
-                </template>
-            </a-table-column>
-        </template>
-    </a-table>
-    <!-- 查看配图模态框 -->
-    <a-modal v-model:visible="visibleImg" title="查看配图" @cancel="handleCancelImg">
-        <div v-if="imgUrl">
-            <img :src="imgUrl" alt="用户头像" style="width: 100%; height: auto" />
-        </div>
-        <div v-else>
-            <p>暂无配图</p>
-        </div>
-    </a-modal>
-    <!-- 设置管理员 -->
-    <a-modal v-model:visible="visibleSetUp" title="设置管理员" @cancel="handleCancelSetUp" @ok="handleOkSetUp">
-        <a-select v-model="adminStatus" :style="{ width: '400px' }" placeholder="选择" allow-clear>
-            <a-option value="普通用户">普通用户</a-option>
-            <a-option value="用户信息管理员">用户信息管理员</a-option>
-            <a-option value="评论体信息管理员">评论体信息管理员</a-option>
-            <a-option value="评论与提问信息管理员">评论与提问信息管理员</a-option>
-        </a-select>
-    </a-modal>
-    <!-- 修改用户信息模态框 -->
-    <a-modal v-model:visible="visibleEdit" title="修改用户信息" @cancel="handleCancelEdit" @ok="handleOk">
-        <a-form ref="form" :model="form">
-            <a-form-item label="昵称" field="nickname">
-                <a-input v-model="form.nickname" />
-            </a-form-item>
-            <a-form-item label="密码" field="password">
-                <a-input v-model="form.password" type="password" />
-            </a-form-item>
-            <a-form-item label="手机号" field="phone">
-                <a-input v-model="form.phone" type="phone" />
-            </a-form-item>
-            <a-form-item label="邮箱" field="email">
-                <a-input v-model="form.email" type="email" />
-            </a-form-item>
-            <a-form-item label="学号" field="stuId">
-                <a-input v-model="form.stuId" type="stuId" />
-            </a-form-item>
-            <a-form-item label="头像" field="avatar">
-                <a-upload v-model:file-list="fileList" :before-upload="beforeUpload" :on-remove="removeFile">
-                    <a-button>上传图片</a-button>
-                </a-upload>
-            </a-form-item>
-        </a-form>
-    </a-modal>
+    <div class="flex flex-col h-full">
+        <a-table :data="currentTableData" :pagination="false" :bordered="{ cell: true }" style="height: 100%" :scroll="{ x: 2000 }" size="large">
+            <template #columns>
+                <a-table-column title="账号" data-index="account" fixed="left" width="150"></a-table-column>
+                <a-table-column title="昵称" data-index="nickname" width="150"></a-table-column>
+                <a-table-column title="头像" width="50">
+                    <template #cell="{ record }">
+                        <a-button size="mini" status="warning" type="text" @click="imgUser(record)">查看</a-button>
+                    </template>
+                </a-table-column>
+                <a-table-column title="创建时间" data-index="created_at" width="400"></a-table-column>
+                <a-table-column title="操作" width="100" fixed="right">
+                    <template #cell="{ record }">
+                        <a-popconfirm content="是否要暂时删除?" ok-text="是" cancel-text="否" @ok="banUser(record)">
+                            <a-button v-if="record.is_forbidden === 0" size="mini" status="danger" type="text">封禁</a-button>
+                        </a-popconfirm>
+                        <a-popconfirm content="是否要恢复此用户?" ok-text="是" cancel-text="否" @ok="unbanUser(record)">
+                            <a-button v-if="record.is_forbidden === 1" size="mini" status="success" type="text">恢复</a-button>
+                        </a-popconfirm>
+                        <a-button size="mini" status="danger" type="text" @click="editUser(record)">修改</a-button>
+                        <a-button size="mini" status="warning" type="text" @click="setUp(record)">设置</a-button>
+                    </template>
+                </a-table-column>
+                <a-table-column title="状态" width="50" data-index="is_forbidden" fixed="right">
+                    <template #cell="{ record }">
+                        <a-button v-if="record.is_forbidden === 0" status="success" size="mini" type="outline">正常</a-button>
+                        <a-button v-if="record.is_forbidden !== 0" status="danger" size="mini" type="outline">封禁</a-button>
+                    </template>
+                </a-table-column>
+            </template>
+        </a-table>
+        <!-- 查看配图模态框 -->
+        <a-modal v-model:visible="visibleImg" title="查看配图" @cancel="handleCancelImg">
+            <div v-if="imgUrl">
+                <img :src="imgUrl" alt="用户头像" style="width: 100%; height: auto" />
+            </div>
+            <div v-else>
+                <p>暂无配图</p>
+            </div>
+        </a-modal>
+        <!-- 设置管理员 -->
+        <a-modal v-model:visible="visibleSetUp" title="设置管理员" @cancel="handleCancelSetUp" @ok="handleOkSetUp">
+            <a-select v-model="adminStatus" :style="{ width: '400px' }" placeholder="选择" allow-clear>
+                <a-option value="普通用户">普通用户</a-option>
+                <a-option value="用户信息管理员">用户信息管理员</a-option>
+                <a-option value="评论体信息管理员">评论体信息管理员</a-option>
+                <a-option value="评论与提问信息管理员">评论与提问信息管理员</a-option>
+            </a-select>
+        </a-modal>
+        <!-- 修改用户信息模态框 -->
+        <a-modal v-model:visible="visibleEdit" title="修改用户信息" @cancel="handleCancelEdit" @ok="handleOk">
+            <a-form ref="form" :model="form">
+                <a-form-item label="昵称" field="nickname">
+                    <a-input v-model="form.nickname" />
+                </a-form-item>
+                <a-form-item label="密码" field="password">
+                    <a-input v-model="form.password" type="password" />
+                </a-form-item>
+                <a-form-item label="手机号" field="phone">
+                    <a-input v-model="form.phone" type="phone" />
+                </a-form-item>
+                <a-form-item label="邮箱" field="email">
+                    <a-input v-model="form.email" type="email" />
+                </a-form-item>
+                <a-form-item label="学号" field="stuId">
+                    <a-input v-model="form.stuId" type="stuId" />
+                </a-form-item>
+                <a-form-item label="头像" field="avatar">
+                    <a-upload v-model:file-list="fileList" :before-upload="beforeUpload" :on-remove="removeFile">
+                        <a-button>上传图片</a-button>
+                    </a-upload>
+                </a-form-item>
+            </a-form>
+        </a-modal>
 
-    <div class="search-bar">
-        <a-pagination :total="total" :page-size="pageSize" :current="currentPage" show-total show-jumper show-page-size @change="handlePageChange" @page-size-change="handlePageSizeChange" />
-        <a-input v-model="searchQuery" placeholder="搜索用户" style="width: 200px" @input="handleSearch" />
+        <!--使用Tailwind CSS  -->
+        <div class="flex justify-between items-center mb-2 p-2">
+            <a-pagination :total="total" :page-size="pageSize" :current="currentPage" show-total show-jumper show-page-size @change="handlePageChange" @page-size-change="handlePageSizeChange" />
+            <a-input v-model="searchQuery" placeholder="搜索评论" class="ml-auto" style="width: 200px" @input="handleSearch" />
+        </div>
     </div>
 </template>
 
 <script>
-import user from '../../api/user';
+import user from '@/api/user';
 
 export default {
     name: 'USER',
@@ -243,11 +246,5 @@ export default {
 </script>
 
 <style scoped>
-.search-bar {
-    margin-bottom: 10px;
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    justify-content: space-between;
-}
+/* 样式 */
 </style>
